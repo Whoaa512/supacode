43916e2822219939013a6d6766f8b93fdde57bdb
CJ Winslow <github@cjwinslow.com>
2026-04-17 13:42:50 -0700

    Replace base ref picker with searchable autocomplete
    The dropdown picker doesn't scale well when repos have many branches.
    A typable search field with filtered suggestions makes it faster to
    find the target ref.

--- supacode/Features/Repositories/Reducer/RepositoriesFeature.swift
+++ supacode/Features/Repositories/Reducer/RepositoriesFeature.swift
@@ -839,6 +839,7 @@ struct RepositoriesFeature {
           baseRefOptions: baseRefOptions,
           branchName: "",
           selectedBaseRef: selectedBaseRef,
+          baseRefSearchText: selectedBaseRef ?? "",
           fetchOrigin: promptSettingsFile.global.fetchOriginBeforeWorktreeCreation,
           validationMessage: nil
         )
